3155c8cd206ec8b0a701883720905902ae54617f
Martin Estevez <martin.estevez@udla.edu.ec>
2026-06-01 09:50:49 -0500

    calculo de promedios por estudiante y asignatura

--- notas.c
+++ notas.c
@@ -5,6 +5,8 @@
 #define NOTA_APR 6.0
 
 void pedirNotas(int nEst, float notas[][N_ASIG]);
+void calcularPromediosEstudiante(int nEst, float notas[][N_ASIG], float promEst[]);
+void calcularPromediosAsignatura(int nEst, float notas[][N_ASIG], float promAsig[]);
 
 int main() {
     int nEst;
@@ -29,6 +31,18 @@ int main() {
     int rep[N_ASIG];
     
     pedirNotas(nEst, notas);
+    calcularPromediosEstudiante(nEst, notas, promEst);
+    calcularPromediosAsignatura(nEst, notas, promAsig);
+    
+    printf("\n=== PROMEDIOS ===\n");
+    printf("\n-- Por Estudiante --\n");
+    for (int i = 0; i < nEst; i++) {
+        printf("Estudiante %d: %.2f\n", i + 1, promEst[i]);
+    }
+    printf("\n-- Por Asignatura --\n");
+    for (int j = 0; j < N_ASIG; j++) {
+        printf("Asignatura %d: %.2f\n", j + 1, promAsig[j]);
+    }
     
     return 0;
 }
@@ -45,5 +59,25 @@ void pedirNotas(int nEst, float notas[][N_ASIG]) {
                 }
             } while (notas[i][j] < 0 || notas[i][j] > 10);
         }
+    }
+}
+
+void calcularPromediosEstudiante(int nEst, float notas[][N_ASIG], float promEst[]) {
+    for (int i = 0; i < nEst; i++) {
+        float suma = 0;
+        for (int j = 0; j < N_ASIG; j++) {
+            suma += notas[i][j];
+        }
+        promEst[i] = suma / N_ASIG;
+    }
+}
+
+void calcularPromediosAsignatura(int nEst, float notas[][N_ASIG], float promAsig[]) {
+    for (int j = 0; j < N_ASIG; j++) {
+        float suma = 0;
+        for (int i = 0; i < nEst; i++) {
+            suma += notas[i][j];
+        }
+        promAsig[j] = suma / nEst;
     }
 }
